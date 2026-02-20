c90db909ee72790f3bbc4a455f8da5156c13e6bb
ds-carlo <carlo.fornoles@devsoft.ph>
2026-02-20 10:45:35 +0800

    initial commit

---
+++ CS/DxFileInput.CustomErrorMessage/Components/Shared/FileInputListEntry.cs
@@ -0,0 +1,32 @@
+﻿using DevExpress.Blazor;
+
+namespace DxFileInput.CustomErrorMessage.Components.Shared;
+
+public class FileInputListEntry
+{
+    public UploadFileInfo UploadInfo { get; init; }
+    public IFileInputSelectedFile SelectedFile { get; set; }
+
+    public string Name => UploadInfo.Name;
+    public string Guid => UploadInfo.Guid;
+    public int Size => (int)UploadInfo.Size;
+
+    public int BytesRead { get; set; }
+    public string? ErrorMessage { get; set; }
+    public byte[]? Data { get; set; }
+
+    public UploadState State() => (
+        SelectedFile is null ? UploadState.NotStarted :
+        !String.IsNullOrEmpty(ErrorMessage) ? UploadState.Error :
+        BytesRead < Size ? UploadState.InProgress :
+        UploadState.Success
+    );
+}
+
+public enum UploadState
+{
+    NotStarted,
+    InProgress,
+    Success,
+    Error
+}
